915a3511c88c628b6d5b78c96e95855f79b465b4
Szymon Konieczny <szymon_konieczny@o2.pl>
2023-08-20 16:27:52 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+let galleryListArray = [];
+
+galleryItems.forEach(galleryItem => {
+  const listItem = document.createElement('li');
+  //   listItem.classList.add('gallery__item');
+  const innerString = `<a class="gallery__item" href="${galleryItem.original}">
+<img
+  class="gallery__image"
+  src="${galleryItem.preview}"
+  
+  alt="${galleryItem.description}"
+/>
+</a>`;
+  listItem.insertAdjacentHTML('beforeend', innerString);
+
+  galleryListArray.push(listItem);
+});
+gallery.append(...galleryListArray);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 console.log(galleryItems);
